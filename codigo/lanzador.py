e64218a138b27454f16823cb971269f7c274d602
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-10-12 18:05:32 +0200

    lanzador

---
+++ codigo/lanzador.py
@@ -0,0 +1,17 @@
+from codigo.ecuaciones import ecuacion1, ecuacion2, ecuacion3, ecuacion4
+from introducir.numero import solicitar_introducir_numero_extremo
+
+def ejercicio():
+    eleccion = solicitar_introducir_numero_extremo("¿Qué ecuación deseas resolver?", 1, 4)
+    if eleccion == 1:
+        print("La solucion a la primera ecuación propuesta es: \n")
+        ecuacion1()
+    elif eleccion == 2:
+        print("La solucion a la segunda ecuación propuesta es: \n")
+        ecuacion2()
+    elif eleccion == 3:
+        print("La solucion a la tercera ecuación propuesta es: \n")
+        ecuacion3()
+    elif eleccion == 4:
+        print("La solucion a la cuarta ecuación propuesta es: \n")
+        ecuacion4()
