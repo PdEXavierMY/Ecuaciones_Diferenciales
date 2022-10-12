5a8a4d5914839964579daf348cc978894d5384f1
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-10-12 18:06:50 +0200

    ajustes

--- codigo/lanzador.py
+++ codigo/lanzador.py
@@ -4,14 +4,14 @@ from introducir.numero import solicitar_introducir_numero_extremo
 def ejercicio():
     eleccion = solicitar_introducir_numero_extremo("¿Qué ecuación deseas resolver?", 1, 4)
     if eleccion == 1:
-        print("La solucion a la primera ecuación propuesta es: \n")
+        print("La solución a la primera ecuación propuesta es: \n")
         ecuacion1()
     elif eleccion == 2:
-        print("La solucion a la segunda ecuación propuesta es: \n")
+        print("La solución a la segunda ecuación propuesta es: \n")
         ecuacion2()
     elif eleccion == 3:
-        print("La solucion a la tercera ecuación propuesta es: \n")
+        print("La solución a la tercera ecuación propuesta es: \n")
         ecuacion3()
     elif eleccion == 4:
-        print("La solucion a la cuarta ecuación propuesta es: \n")
+        print("La solución a la cuarta ecuación propuesta es: \n")
         ecuacion4()
