d9006a4900a2483d7e1bb7f8cbbea51a7610c646
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-10-12 13:30:03 +0200

    ec 2

--- ecuaciones.py
+++ ecuaciones.py
@@ -1,11 +1,23 @@
 #crea una funcion que resuelva una ecuacion diferencial
 import sympy as sp
-def ecuacion():
+import math
+def ecuacion1():
     x = sp.Symbol('x')
     y = sp.Function('y')
-    f = (x**2*(y(x))-(y(x)))/(y(x)+1)
-    sp.Eq(y(x).diff(x), f)
+    f = ((x**2)*(y(x))-(y(x)))/((y(x))+1)
+    sp.Eq(y(x).diff(), f)
+    ics = {y(3): -1}
     sol = sp.dsolve(y(x).diff(x) - f)
     print(sol)
 
-ecuacion()
+ecuacion1()
+
+def ecuacion2():
+    x = sp.Symbol('x')
+    y = sp.Function('y')
+    f = y(x)*float(math.log(y(x)))
+    sp.Eq(y(x).diff()*math.sin(x), f)
+    sol = sp.dsolve(y(x).diff(x) - f)
+    print(sol)
+
+ecuacion2()
